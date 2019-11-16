3395fb3862a3587db1a300a5e9ed9d97ca71898b
nico_martin <mail@nicomartin.ch>
2019-11-16 10:11:25 +0100

    sw manifest

--- src/Components/Profile/index.jsx
+++ src/Components/Profile/index.jsx
@@ -52,7 +52,18 @@ export default (
             style="nobutton"
             text="App herunterladen"
             icon="mdi/download"
-            large
+            onClick={() => {
+              window.installEvent.prompt();
+              window.installEvent.userChoice.then(choiceResult => {
+                if (choiceResult.outcome === 'accepted') {
+                  console.log('User accepted the A2HS prompt');
+                  document.body.classList.remove('can-install');
+                } else {
+                  console.log('User dismissed the A2HS prompt');
+                }
+                window.installEvent = null;
+              });
+            }}
           />
         </div>
       )}
